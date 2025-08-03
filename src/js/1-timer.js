2dc3cb030759e7cb4dc6a3474e18e6cfdaf85d28
Azazello <donazazello1312@gmail.com>
2025-08-03 21:15:17 +0300

    fix timer js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,10 +10,10 @@ startBtn.disabled = true;
 let userSelectedDate = null; //! select date in milliseconds
 let saveInterval = null; //! interval time in milliseconds
 
-const days = document.querySelector('span[data-days]');
-const hours = document.querySelector('span[data-hours]');
-const minutes = document.querySelector('span[data-minutes]');
-const seconds = document.querySelector('span[data-seconds]');
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minutesEl = document.querySelector('span[data-minutes]');
+const secondsEl = document.querySelector('span[data-seconds]');
 
 const options = {
   enableTime: true,
@@ -21,58 +21,75 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    startActive(userSelectedDate);
-    console.log(selectedDates[0]);
+    if (selectedDates[0] < options.defaultDate) {
+      return iziToast.error({
+        color: 'red',
+        message: 'Please select a future date.',
+        position: 'topRight',
+        close: false,
+      });
+    }
+    userSelectedDate = selectedDates[0];
+    startBtn.disabled = false;
+    // startActive(userSelectedDate);
+    //   console.log(selectedDates[0]);
+    //   console.log(options.defaultDate - selectedDates[0]);
   },
 };
 
 flatpickr(dateInput, options);
 
-dateInput.addEventListener('input', saveValue);
+// dateInput.addEventListener('input', saveValue);
 
 startBtn.addEventListener('click', start);
 
-function saveValue() {
-  let a = flatpickr.parseDate(dateInput.value);
-  userSelectedDate = a.getTime();
-  intervalTime(userSelectedDate);
-}
+// function saveValue(e) {
+//   console.log(e.currentTarget);
+//   let a = flatpickr.parseDate(dateInput.value);
+//   userSelectedDate = a.getTime();
+//   intervalTime(userSelectedDate);
+// }
 
-function startActive(a) {
-  if (a > Date.now()) {
-    startBtn.disabled = false;
-  } else {
-    startBtn.disabled = true;
-    iziToast.error({
-      color: 'red',
-      message: 'Please select a future date.',
-      position: 'topRight',
-      close: false,
-    });
-  }
-}
+// function startActive(a) {
+//   if (a > Date.now()) {
+//     startBtn.disabled = false;
+//   } else {
+//     startBtn.disabled = true;
+//
+//   }
+// }
 
-function intervalTime(a) {
-  saveInterval = a - Date.now();
-} //! in a put select date in milliseconds, a = number; (userSelectedDate)
+// function intervalTime(a) {
+//   saveInterval = a - Date.now();
+// } //! in a put select date in milliseconds, a = number; (userSelectedDate)
 
 function start() {
   updateInterval();
 }
 
 function updateInterval() {
-  let intervalId = setInterval(() => {
-    saveInterval -= 1000;
-    showDateHtml(convertMs(saveInterval));
-    if (saveInterval > 0) {
-      startBtn.disabled = true;
-      dateInput.disabled = true;
-    } else {
-      clearInterval(intervalId);
-      startBtn.disabled = false;
-      dateInput.disabled = false;
-    }
+  setInterval(() => {
+    const currentDate = Date.now();
+    let diff = userSelectedDate - currentDate;
+    const { days, hours, minutes, seconds } = convertMs(diff);
+    console.log(String(days).padStart(2, '0'));
+    daysEl.textContent = String(days).padStart(2, '0');
+    hoursEl.textContent = String(hours).padStart(2, '0');
+    minutesEl.textContent = String(minutes).padStart(2, '0');
+    secondsEl.textContent = String(seconds).padStart(2, '0');
   }, 1000);
+  // let intervalId = setInterval(() => {
+  //   diff -= 1000;
+  //   showDateHtml(convertMs(diff));
+  //   if (diff > 0) {
+  //     startBtn.disabled = true;
+  //     dateInput.disabled = true;
+  //   } else {
+  //     clearInterval(intervalId);
+  //     startBtn.disabled = false;
+  //     dateInput.disabled = false;
+  //   }
+  // }, 1000);
 }
 
 function convertMs(ms) {
@@ -94,9 +111,4 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-function showDateHtml(data) {
-  days.textContent = String(data.days).padStart(2, '0');
-  hours.textContent = String(data.hours).padStart(2, '0');
-  minutes.textContent = String(data.minutes).padStart(2, '0');
-  seconds.textContent = String(data.seconds).padStart(2, '0');
-}
+function showDateHtml(data) {}
